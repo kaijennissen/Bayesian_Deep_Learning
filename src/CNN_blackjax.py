8b3ed36b25abe5f60a248b9bb1f4a684f3ee386d
Kai Jennissen <k_jennissen@icloud.com>
2022-01-24 15:13:39 +0100

    added CNN in blackjax

--- src/CNN_blackjax.py
+++ src/CNN_blackjax.py
@@ -1,13 +1,17 @@
-import pickle
 import time
+import warnings
 from functools import partial
+
+warnings.simplefilter("ignore", FutureWarning)
+import pickle
 
 import blackjax.nuts as nuts
 import blackjax.stan_warmup as stan_warmup
+import flax.linen as nn
 import jax
-import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
+from numpyro.contrib.module import random_flax_module
 from numpyro.infer.util import initialize_model
 
 with open("data/mnist_train.pickle", "rb") as file:
@@ -17,51 +21,40 @@ with open("data/mnist_test.pickle", "rb") as file:
 
 x_train, y_train = train["image"] / 255, train["label"] * 1.0
 x_test, y_test = test["image"] / 255, test["label"] * 1.0
-x_train = x_train.reshape(x_train.shape[0], -1)
-x_test = x_test.reshape(x_test.shape[0], -1)
 
 
-@jax.jit
-def nonlin(x):
-    return jax.nn.relu(x)
+class CNN(nn.Module):
+    """LeNet"""
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=4, kernel_size=(5, 5))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(6, 6), strides=(2, 2))
+        x = x.reshape((x.shape[0], -1))  # flatten
+        x = nn.Dense(features=10)(x)
+        x = nn.log_softmax(x)
+        return x
 
 
-def GaussianBNN(X, y=None):
-
-    N, feature_dim = X.shape
-    out_dim = 10
-    layer1_dim = 64
-    layer2_dim = 64
-
-    # layer 1
-    W1 = numpyro.sample("W1", dist.Normal(jnp.zeros((feature_dim, layer1_dim)), 1.0))
-    b1 = numpyro.sample("b1", dist.Normal(jnp.zeros(layer1_dim), 1.0))
-    out1 = nonlin(jnp.matmul(X, W1)) + b1
-
-    # layer 2
-    W2 = numpyro.sample("W2", dist.Normal(jnp.zeros((layer1_dim, layer2_dim)), 1.0))
-    b2 = numpyro.sample("b2", dist.Normal(jnp.zeros(layer1_dim), 1.0))
-    out2 = nonlin(jnp.matmul(out1, W2)) + b2
-
-    # output layer
-    W3 = numpyro.sample("out_layer", dist.Normal(jnp.zeros((layer2_dim, out_dim)), 1.0))
-    b3 = numpyro.sample("b3", dist.Normal(jnp.zeros(out_dim), 1.0))
-
-    logits = numpyro.deterministic("mean", jnp.dot(out2, W3) + b3)
-
-    assert logits.shape == (N, 10)
+def BayesianCNN(X, y=None):
+    N, *_ = X.shape
+    module = CNN()
+    net = random_flax_module(
+        "nn", module, dist.Normal(0.0, 10.0), input_shape=(1, 28, 28, 1)
+    )
+    logits = net(X)
     if y is not None:
         assert y.shape == (N,)
+    assert logits.shape == (N, 10)
+    numpyro.sample("y", dist.Categorical(logits=logits), obs=y)
 
-    with numpyro.plate("data", size=N, dim=-2):
-        numpyro.sample("y", dist.Categorical(logits=logits), obs=y)
 
-
-rng_key = jax.random.PRNGKey(0)
+rng_key = jax.random.PRNGKey(2643)
 # Step 1: get the log-probability of the model
 init_params, potential_fn_gen, *_ = initialize_model(
     rng_key,
-    GaussianBNN,
+    BayesianCNN,
     model_args=(x_test, y_test),
     dynamic_args=True,
 )
@@ -78,7 +71,7 @@ kernel_factory = lambda step_size, inverse_mass_matrix: nuts.kernel(
 print("start warmup")
 start_time = time.time()
 last_state, (step_size, inverse_mass_matrix), _ = stan_warmup.run(
-    rng_key, kernel_factory, initial_state, 1000
+    rng_key, kernel_factory, initial_state, 2
 )
 end_time = time.time() - start_time
 print(f"finished warmup in : {end_time}")
