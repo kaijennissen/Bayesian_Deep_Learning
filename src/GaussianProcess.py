f8549393dbb081106fa808d6a7d032bd452253b4
Kai Jennissen <k_jennissen@icloud.com>
2022-02-14 10:58:34 +0100

    added hetroscedastic datasets

--- src/GaussianProcess.py
+++ src/GaussianProcess.py
@@ -2,7 +2,6 @@ import argparse
 import warnings
 from datetime import datetime
 from functools import partial
-from typing import Tuple
 
 import pandas as pd
 
@@ -13,19 +12,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpyro
 import numpyro.distributions as dist
-from jax import jit, random, vmap
-from numpyro import handlers
 from numpyro.diagnostics import hpdi
-from numpyro.infer import (
-    MCMC,
-    NUTS,
-    Predictive,
-    init_to_feasible,
-    init_to_median,
-    init_to_sample,
-    init_to_uniform,
-    init_to_value,
-)
+from numpyro.infer import MCMC, NUTS, Predictive, init_to_median
 
 numpyro.set_host_device_count(4)
 
@@ -165,7 +153,7 @@ def predict(rng_key, X, Y, X_test, var, length, noise):
     k_XX = rbf_kernel(X=X, Z=X.T, var=var, length=length, noise=noise)
     K_xx_inv = jnp.linalg.solve(k_XX, jnp.eye(k_XX.shape[0]))
     K = k_pp - jnp.matmul(k_pX, jnp.matmul(K_xx_inv, jnp.transpose(k_pX)))
-    sigma_noise = jnp.sqrt(jnp.clip(jnp.diag(K), a_min=0.0)) * random.normal(
+    sigma_noise = jnp.sqrt(jnp.clip(jnp.diag(K), a_min=0.0)) * jax.random.normal(
         rng_key, X_test.shape[:1]
     )
 
@@ -175,23 +163,69 @@ def predict(rng_key, X, Y, X_test, var, length, noise):
     return mean, mean + sigma_noise, K
 
 
-def get_data(N: int = 100, N_test: int = 200):
+def get_data(dataset: str = "M", N: int = 100, N_test: int = 200):
+    x, y = globals()[f"dataset_{dataset}"]()
+    x_test = jnp.asarray(np.linspace(1.1 * np.min(x), 1.1 * np.max(x), N_test))
+    return x[:, np.newaxis], x_test[:, np.newaxis], y
+
+
+def dataset_M(N: int = 100):
     df = pd.read_csv("./data/motor.csv")
     x = jnp.asarray(df["times"])
     y = jnp.asarray(df["accel"].values)
 
     x = (x - np.mean(x)) / np.std(x)
     y = (y - np.mean(y)) / np.std(y)
-    x_test = jnp.asarray(np.linspace(1.1 * np.min(x), 1.1 * np.max(x), N_test))
-
-    return x[:, np.newaxis], x_test[:, np.newaxis], y
+    return x, y
 
 
-def main(N: int, num_warmup: int, num_samples: int, num_chains: int, post_samples: int):
+def dataset_G(N: int = 100):
+    # Goldberg et al. 1998
+    x = np.random.uniform(0, 1, N)
+    mean = 2 * np.sin(2 * np.pi * x)
+    sd = 0.5 + x
+    y = mean + sd * np.random.normal(size=N)
+    return x, y
 
-    X, X_test, y = get_data(N=N)
 
-    rng_key, rng_key_predict = random.split(random.PRNGKey(532))
+def dataset_Y(N: int = 100):
+    # Yuan and Wahba 2004
+    x = np.random.uniform(0, 1, N)
+    mean = 2 * (np.exp(-30 * (x - 0.25) ** 2) + np.sin(np.pi * x**2)) - 2
+    sd = np.sin(2 * np.pi * x)
+    y = mean + np.exp(sd) * np.random.normal(size=N)
+    return x, y
+
+
+def dataset_W(N: int = 200):
+    # Williams 1996
+    x = np.random.uniform(0, np.pi, N)
+    mean = np.sin(2.5 * x) * np.sin(1.5 * x)
+    sd = 0.01 + 0.25 * (1 - np.sin(2.5 * x)) ** 2
+    y = mean + np.exp(sd) * np.random.normal(size=N)
+    return x, y
+
+
+def dataset_L(N: int = 100):
+    df = pd.read_csv("data/lidar.csv")
+    x = df.range.values
+    y = df.logratio.values
+    x = (x - np.mean(x)) / np.std(x)
+    y = (y - np.mean(y)) / np.std(y)
+    return x, y
+
+
+def main(
+    N: int,
+    dataset: str,
+    num_warmup: int,
+    num_samples: int,
+    num_chains: int,
+    post_samples: int,
+):
+
+    X, X_test, y = get_data(dataset=dataset)
+
     nuts_kernel = NUTS(GP2, init_strategy=init_to_median())
     mcmc = MCMC(
         nuts_kernel,
@@ -201,20 +235,20 @@ def main(N: int, num_warmup: int, num_samples: int, num_chains: int, post_sample
         chain_method="parallel",
         jit_model_args=True,
     )
-    mcmc.run(X=X, y=y, rng_key=rng_key)
+    mcmc.run(X=X, y=y, rng_key=jax.random.PRNGKey(532))
     mcmc.print_summary()
     samples = mcmc.get_samples()
-    idx = random.randint(
-        random.PRNGKey(12), shape=(post_samples,), minval=0, maxval=num_samples
+    idx = jax.random.randint(
+        jax.random.PRNGKey(12), shape=(post_samples,), minval=0, maxval=num_samples
     )
     subsamples = map(
         lambda x: x[idx],
         (samples["var_k1"], samples["length_k1"], samples["noise_k1"]),
     )
-    vmap_args = (random.split(jax.random.PRNGKey(324), post_samples),) + tuple(
+    vmap_args = (jax.random.split(jax.random.PRNGKey(324), post_samples),) + tuple(
         subsamples
     )
-    mu, predictions, K1 = vmap(
+    mu, predictions, K1 = jax.vmap(
         lambda rng_key, var, length, noise: predict(
             rng_key, X, y, X_test, var, length, noise
         )
@@ -224,10 +258,10 @@ def main(N: int, num_warmup: int, num_samples: int, num_chains: int, post_sample
         lambda x: x[idx],
         (samples["var_k2"], samples["length_k2"], samples["noise_k2"]),
     )
-    vmap_args = (random.split(jax.random.PRNGKey(675), post_samples),) + tuple(
+    vmap_args = (jax.random.split(jax.random.PRNGKey(675), post_samples),) + tuple(
         subsamples
     )
-    log_scale, predictions, K2 = vmap(
+    log_scale, predictions, K2 = jax.vmap(
         lambda rng_key, var, length, noise: predict(
             rng_key,
             X,
@@ -281,13 +315,14 @@ def main(N: int, num_warmup: int, num_samples: int, num_chains: int, post_sample
     plt.legend(loc="lower right")
 
     datetime_str = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    plt.savefig(f"plots/GaussianProcess_{datetime_str}.jpg", dpi=300)
+    plt.savefig(f"plots/GaussianProcess_{dataset}.jpg", dpi=300)
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--sample-size", default=50, type=int)
+    parser.add_argument("--dataset", default="M", type=str)
     parser.add_argument("--num-warmup", default=1_000, type=int)
     parser.add_argument("--num-samples", default=4_000, type=int)
     parser.add_argument("--num-chains", default=1, type=int)
@@ -296,6 +331,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(
         N=args.sample_size,
+        dataset=args.dataset,
         num_warmup=args.num_warmup,
         num_samples=args.num_samples,
         num_chains=args.num_chains,
